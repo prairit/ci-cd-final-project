3045830943860108d2cb3a6c9c24be336a626fce
prairit <shivamverma64@gmail.com>
2024-05-19 12:33:02 -0400

    lint fixes v6

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -49,7 +49,7 @@ def not_found(error):
             status=status.HTTP_404_NOT_FOUND,
             error="Not Found",
             message=message),
-            status.HTTP_404_NOT_FOUND,
+        status.HTTP_404_NOT_FOUND,
     )
 
 
